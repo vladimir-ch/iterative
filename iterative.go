9e71a7f4fe0a8be14b6ad0dbb2bc2ec6b85fef0a
Vladimir Chalupecky <vladimir.chalupecky@gmail.com>
2017-04-21 07:13:37 +0200

    Add documentation

--- iterative.go
+++ iterative.go
@@ -131,21 +131,56 @@ const (
 	EndIteration
 )
 
-// Method is an iterative method that produces a sequence x_i of vectors of
-// dimension dim converging to the vector x satisfying an dim×dim system of
-// linear equations
-//  A x = b.
+// Method is an iterative method that produces a sequence of vectors converging
+// to the vector x satisfying a system of linear equations
+//  A x = b,
+// where A is non-singular dim×dim matrix, and x and b are vectors of dimension
+// dim.
+//
+// Method uses a reverse-communication interface between the iterative algorithm
+// and the caller. Method acts as a client that commands the caller to perform
+// needed operations via Operation returned from Iterate methods. This provides
+// independence of Method on representation of the matrix A, and enables
+// automation of common operations like checking for convergence and maintaining
+// statistics.
 type Method interface {
+	// Init initializes the method for solving an dim×dim linear system.
 	Init(dim int)
+
+	// Iterate retrieves data from Context, updates it, and returns the next
+	// operation. The caller must perform the Operation using data in
+	// Context, and depending on the state call Iterate again.
 	Iterate(*Context) (Operation, error)
 }
 
+// Context mediates the communication between a Method and the caller. It must
+// not be modified or accessed apart from the commanded Operations.
 type Context struct {
-	X            []float64
-	Residual     []float64
+	// X is the current approximate solution. On the first call to
+	// Method.Iterate, X must contain the initial estimate. Method must
+	// update X with the current estimate when it commands ComputeResidual
+	// and EndIteration.
+	X []float64
+	// Residual is the current residual b-A*x. On the first call to
+	// Method.Iterate, Residual must contain the initial residual.
+	// TODO(vladimir-ch): Consider whether the behavior should also include:
+	// Method must update Residual with the current value of b-A*x when it
+	// commands EndIteration.
+	Residual []float64
+	// ResidualNorm is (an estimate of) the norm of the current residual.
+	// Method must update it when it commands CheckResidualNorm. It does
+	// not have to be equal to the norm of Residual, some methods (e.g.,
+	// GMRES) can estimate the residual norm without forming the residual
+	// itself.
 	ResidualNorm float64
-	Converged    bool
+	// Converged indicates to Method that the ResidualNorm satisfies the
+	// stopping criterion as a result of CheckResidualNorm operation.
+	// If a Method commands EndIteration with Converged true, the caller
+	// must not call Method.Iterate again without calling Method.Init first.
+	Converged bool
 
+	// Src and Dst are the source and destination vectors for various
+	// Operations.
 	Src, Dst []float64
 }
 
