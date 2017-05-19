f4a5734403282b625140ff0c3b9091d3b43ba27e
Vladimir Chalupecky <vladimir.chalupecky@gmail.com>
2017-05-19 17:46:36 +0200

    Rework test for CG and add test cases from Matrix Market

--- cg_test.go
+++ cg_test.go
@@ -1,3 +1,7 @@
+// Copyright ©2017 The gonum Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package iterative
 
 import (
@@ -5,54 +9,56 @@ import (
 	"math/rand"
 	"testing"
 
-	"github.com/gonum/blas"
-	"github.com/gonum/blas/blas64"
 	"github.com/gonum/floats"
 )
 
 func TestCG(t *testing.T) {
 	rnd := rand.New(rand.NewSource(1))
-	for _, n := range []int{1, 2, 3, 4, 5, 10, 20, 50, 100, 200, 500} {
-		// Generate a symmetric positive-definite matrix A.
-		a := make([]float64, n*n)
-		lda := n
-		for i := 0; i < n; i++ {
-			for j := i; j < n; j++ {
-				a[i*lda+j] = rnd.Float64()
-			}
-		}
-		for i := 0; i < n; i++ {
-			a[i*lda+i] += float64(n)
-		}
+	for _, tc := range []testCase{
+		randomSPD(1, rnd),
+		randomSPD(2, rnd),
+		randomSPD(3, rnd),
+		randomSPD(4, rnd),
+		randomSPD(5, rnd),
+		randomSPD(10, rnd),
+		randomSPD(20, rnd),
+		randomSPD(50, rnd),
+		randomSPD(100, rnd),
+		randomSPD(200, rnd),
+		randomSPD(500, rnd),
+		market("nos1"),
+		market("nos4"),
+		market("nos5"),
+		market("bcsstm20"),
+		market("bcsstm22"),
+	} {
+		n := tc.n
+		A := tc.a
 		// Compute the right-hand side b so that the vector [1,1,...,1]
 		// is the solution.
 		x := make([]float64, n)
 		for i := range x {
 			x[i] = 1
 		}
+		b := make([]float64, n)
+		A.MatVec(b, x)
+		// Initial estimate is the zero vector.
 		want := make([]float64, n)
 		copy(want, x)
-		b := make([]float64, n)
-		bi := blas64.Implementation()
-		bi.Dsymv(blas.Upper, n, 1, a, lda, x, 1, 0, b, 1)
-		// Initial estimate is the zero vector.
 		for i := range x {
 			x[i] = 0
 		}
 
-		A := MatrixOps{
-			MatVec: func(dst, x []float64) {
-				bi.Dsymv(blas.Upper, n, 1, a, lda, x, 1, 0, dst, 1)
-			},
-		}
-		r, err := LinearSolve(A, b, &CG{}, Settings{Tolerance: 1e-14})
-
+		r, err := LinearSolve(A, b, &CG{}, Settings{
+			MaxIterations: tc.iters,
+			Tolerance:     1e-12,
+		})
 		if err != nil {
-			t.Errorf("Case n=%v: unexpected error %v", n, err)
+			t.Errorf("Case %v (n=%v): unexpected error %v", tc.name, n, err)
 		}
 		dist := floats.Distance(r.X, want, math.Inf(1))
-		if dist > 1e-10 {
-			t.Errorf("Case n=%v: unexpected solution, |want-got|=%v", n, dist)
+		if dist > tc.tol {
+			t.Errorf("Case %v (n=%v): unexpected solution, |want-got|=%v", tc.name, n, dist)
 		}
 	}
 }
